a5d3528fd0f341612f620d16b5702ea2fd2942e0
CAG2Mark <markng989@gmail.com>
2021-12-04 13:22:38 +0800

    update get_input

--- get_input.py
+++ get_input.py
@@ -9,4 +9,6 @@ session = open("session").read()
 os.system(f"mkdir -p ./day{day}")
 os.system(f"curl https://adventofcode.com/2021/day/{day}/input --cookie \"session={session}\" -o ./day{day}/input")
 
+print("First line of file:")
+print(open(f"day{day}/input").readline())
 
