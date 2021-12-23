7bca10890e4a2f8a0aa05d6d54cdd0be443b09f1
CAG2Mark <markng989@gmail.com>
2021-12-23 19:25:21 +0800

    day 23, brute force dijkstra's

---
+++ day23/p1.py
@@ -0,0 +1,192 @@
+from copy import deepcopy
+from collections import defaultdict
+import heapq as heap
+
+data = open("input").read().split("\n")
+if not data[-1].strip(): data = data[0:-1]
+# data = [int(x) for x in data]
+
+burrow0 = []
+burrow1 = []
+burrow2 = []
+burrow3 = []
+
+
+hallway = [-1]*11
+
+d = {"A": 0, "B": 1, "C": 2, "D": 3}
+d_ = {0: "A", 1: "B", 2: "C", 3: "D"}
+
+energy = {0: 1, 1: 10, 2: 100, 3: 1000}
+hallwaypos = {0: 0, 1: 1, 2: 3, 3: 5, 4: 7, 5: 9, 6: 10}
+burrowpos = {0: 2, 1: 4, 2: 6, 3: 8}
+burrowpos_rev = {2: 0, 4: 1, 6: 2, 8: 3}
+
+burrow0.append(d[data[3][3]])
+burrow0.append(d[data[2][3]])
+
+burrow1.append(d[data[3][5]])
+burrow1.append(d[data[2][5]])
+
+burrow2.append(d[data[3][7]])
+burrow2.append(d[data[2][7]])
+
+burrow3.append(d[data[3][9]])
+burrow3.append(d[data[2][9]])
+
+# testing
+testing = False
+if testing:
+    burrow0 = [0]
+    burrow1 = [1,1]
+    burrow2 = [2,2]
+    burrow3 = [3,3]
+    hallway = [-1, -1, -1, -1, -1, -1, -1, -1, -1, 0, -1]
+
+burrows = [burrow0, burrow1, burrow2, burrow3]
+def is_complete(burrows):
+    for i, b in enumerate(burrows):
+        if len(b) != 2: return False
+        for a in b:
+            if a != i: return False
+    return True
+
+def can_move(pos1, pos2, hallway):
+    blocked = False
+    for p in range(min(pos1, pos2), max(pos1, pos2)+1):
+        if hallway[p] != -1:
+            blocked = True
+            break
+    if blocked: return False
+    return True
+
+def get_moves(burrows, hallway):
+    possibilities = []
+    for i in range(len(hallway)):
+
+        if i in burrowpos_rev:
+            pos = burrowpos_rev[i]
+            burrow = burrows[burrowpos_rev[i]]
+            # if empty, ignore
+            if not burrow: continue
+            # if burrow is already complete, ignre
+            if len(burrow) == 2 and burrow[0] == pos and burrow[1] == pos: continue
+            # if burrow is partially complete, also ignore
+            if len(burrow) == 1 and burrow[0] == pos: continue
+            if len(burrow) > 2: 
+                print("Error: burrow length > 2")
+                return
+        if i not in burrowpos_rev and hallway[i] == -1: continue
+
+        for j in range(len(hallway)):
+            if i == j: continue
+            # if already filled, just an optimisation
+            if hallway[j] != -1: continue
+            # if og pos already in hallway can't move to a non-burrow position
+            if not i in burrowpos_rev and not j in burrowpos_rev: continue
+
+            # if destination is above brrow and is full, ignore
+            if j in burrowpos_rev and len(burrows[burrowpos_rev[j]]) >= 2: continue
+            
+            # check all positions between cur spot and next spot
+            next_pos = i
+            # if is not in burrow, don't include current pos in the checking range
+            if not i in burrowpos_rev:
+                if j > i: next_pos += 1
+                else: next_pos -= 1
+
+            if not can_move(next_pos, j, hallway): continue
+            bur = deepcopy(burrows)
+
+            hallw = deepcopy(hallway)
+            temp = 0
+
+            dist = abs(j-i)
+
+            # take from current position
+            if i in burrowpos_rev:
+                dist += 1
+                burpos = burrowpos_rev[i]
+                temp = bur[burpos].pop()
+                if len(bur[burpos]) == 0: dist += 1
+            else:
+                temp = hallw[i]
+                hallw[i] = -1
+            # move to next position
+            if j in burrowpos_rev:
+                dist += 1
+                burpos = burrowpos_rev[j]
+
+                if len(bur[burpos]) == 0: 
+                    dist += 1
+                if burpos != temp: continue
+                if bur[burpos] and bur[burpos][0] != temp: continue
+                if len(bur[burpos]) >= 2: continue
+                bur[burpos].append(temp)
+
+            else:
+                hallw[j] = temp
+            cost = energy[temp]
+            cost *= dist
+
+            possibilities.append((cost, (bur, hallw)))
+
+    return possibilities
+
+def dijkstra(burrows, hallway, visited = []):
+    visited = visited.copy()
+    visited.append(str((burrows, hallway)))
+    
+    costs = defaultdict(lambda: float('inf'))
+    costs[str((burrows, hallway))] = 0
+    parents = {}
+
+    pq = []
+    heap.heappush(pq, (0, (burrows, hallway)))
+
+    while pq:
+        cost, node = heap.heappop(pq)
+        visited.append(str(node))
+        # print()
+        # print(str(node), cost)
+        # print("->")
+        if is_complete(node[0]):
+            print(costs[str(node)])
+            return costs, parents, str(node)
+        poss = get_moves(node[0], node[1])
+        for weight, p in poss:
+            if str(p) in visited: continue
+            new_cost = costs[str(node)] + weight
+            # print(p, "cost:", new_cost, "\t increase:", weight) 
+            if costs[str(p)] > new_cost:
+                parents[str(p)] = str(node)
+                costs[str(p)] = new_cost
+                heap.heappush(pq, (new_cost, p))
+
+    return costs, parents, ""
+
+
+costs, parents, last = dijkstra(burrows, hallway)
+path = [last]
+while last != str((burrows, hallway)):
+    last = parents[last]
+    path.insert(0, last)
+
+import ast
+for p in path:
+    a = ast.literal_eval(p)
+    for h in a[1]:
+        print("." if h == -1 else d_[h], end="")
+    print()
+    print("##", end = "")
+    for b in a[0]:
+        if len(b) < 2: print(".#", end="")
+        else: print(d_[b[1]], end="#")
+    print("#")
+    print("##", end="")
+    for b in a[0]:
+        if len(b) == 0: print(".#", end="")
+        else: print(d_[b[0]], end="#")
+    print("#")
+    print("total cost", costs[p])
+
